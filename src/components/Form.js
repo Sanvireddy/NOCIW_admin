f897ab81590ceffd2ea0b2e93ce70f89755b6d66
pooja21082001 <poojapatidar897@gmail.com>
2021-08-08 22:56:33 +0530

    add authentication

--- src/components/Form.js
+++ src/components/Form.js
@@ -6,24 +6,61 @@ import {
   TouchableOpacity,
   View,
 } from 'react-native';
-
+const Name = "Nociw";
+const Password = "12345";
 export default class Form extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userName: "",
+      password: "",
+      userVerification: false,
+    };
+
+    this.setName = this.setName.bind(this);
+    this.setPassword = this.setPassword.bind(this);
+    this.compareUser = this.compareUser.bind(this);
+  }
+  setName(name) {
+    this.setState({ userName: name })
+  }
+  setPassword(pswd) {
+    this.setState({ password: pswd })
+  }
+  compareUser() {
+    console.log(Name,Password,this.state.userName,this.state.password)
+    if (this.state.userName === Name && this.state.password === Password)
+    {
+      this.setState({ userVerification: true })
+      alert("login successfully!")
+    }
+    else {
+      alert("wrong info")
+    }
+      
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <TextInput
+          onChangeText={(userName) => this.setName({userName})}
           style={styles.inputBox}
           underlinColorAndroid="rgba(0,0,0,0)"
-          placeholder="Email"
+          placeholder="UserName"
           placeholderTextColor="#ffffff"
         />
         <TextInput
+          onChangeText={(password)=>this.setPassword({password})}
           style={styles.inputBox}
+          secureTextEntry={true}
           underlinColorAndroid="rgba(0,0,0,0)"
           placeholder="Password"
           placeholderTextColor="#ffffff"
         />
-        <TouchableOpacity style={styles.button}>
+        <TouchableOpacity
+          onPress={() => this.compareUser()}
+          style={styles.button}>
           <Text style={styles.buttonText}>Login</Text>
         </TouchableOpacity>
       </View>
